b3104315a97ad37cba250f9d53c906f1c1556363
Tun Khine <tunkhine126@gmail.com>
2020-02-10 13:35:24 -0500

    Adding task components

---
+++ src/components/Task.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import Task from './Task';
+
+export default {
+  component: Task,
+  title: 'Task',
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+};
+
+export const taskData = {
+  id: '1',
+  title: 'Test Task',
+  state: 'TASK_INBOX',
+  updatedAt: new Date(2020, 0, 1, 9, 0),
+
+  id: '2',
+  title: '2nd Task',
+  state: 'TASK_INBOX',
+  updatedAt: new Date(2020, 0, 1, 9, 0),
+
+};
+
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+export const Default = () => {
+  return <Task task={{ ...taskData }} {...actionsData} />;
+};
+
+export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />;
+
+export const Archived = () => (
+  <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+);
